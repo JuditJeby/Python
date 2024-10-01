792e0b9fe9d2f1d80fb59c4a7e40408783ca504a
JuditJeby <juditjeby2028@cs.sjcetpalai.ac.in>
2024-10-01 15:29:33 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,10 @@
+"""Author=Judit Jeby
+date :01/10/2024
+Used for the finding the sum
+
+"""
+
+number1=int(input("Enter first number :"))
+number2=int(input("Enter second number:"))
+sum=number1+number2
+print(sum)
