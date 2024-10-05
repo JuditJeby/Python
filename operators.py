792259217501af669fece8bf9ed2fc38ebb91a87
JuditJeby <juditjeby2028@cs.sjcetpalai.ac.in>
2024-10-05 17:21:33 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,17 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+sum=a+b
+division=a/b
+print("sum :",sum, end=",")
+print("division :",division)
+
+print("Is a greater then b ?", end="  ")
+print(a>b)
+
+print("Are a and b equal?", end=" " )
+print(a==b)
+print("Logical AND:", end="")
+print( (a>b) and (b<a))
+
+print("Logical OR:",end="")
+print((a>=b) or (b<=a))
---
+++ stuent.py
@@ -0,0 +1,5 @@
+name=(input("Enter your name :"))
+age=(input("Enter your age :"))
+print("Hello,",name,end="!  " )
+print("You are ",age,"years old")
+
