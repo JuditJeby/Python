06e6cb2ba34b33823047a55c480afb6b74cc5656
JuditJeby <juditjeby2028@cs.sjcetpalai.ac.in>
2024-10-08 15:03:29 +0530

    Add files via upload

---
+++ arithmetic operators.py
@@ -0,0 +1,21 @@
+"""
+Author - Judit Jeby
+Created on 08/10/2024
+Program  using arithmetic operators
+"""
+
+num1=int(input("Enter the  first number :"))
+num2=int(input("Enter the second  number :"))
+num3=int(input("Enter the third number :"))
+sum=(num1+num2)
+print("The sum of num1 and num2 is: ",sum)
+subtract=(num2-num1)
+print("The difference when num2 is subtracted from num1 is: ",subtract)
+product=(num2*num1)
+print("The product of num1 and num2 is: ",product)
+division=(num2/num1)
+print("The quotient when num1 is divided by num2 is:",division)
+modulus=(num1%num2)
+print("The remainder when num1 is divided by num2 is:",modulus)
+combined= (num1 + num2) * num3 / 2
+print("he result of (num1 + num2) * num3 / 2 is:",combined)
