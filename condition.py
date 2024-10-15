1c0136ffeecc5bed11b48542bb6abc654b15aa9f
JuditJeby <juditjeby2028@cs.sjcetpalai.ac.in>
2024-10-15 14:05:45 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,15 @@
+"""
+Author-JUdit jeby
+Created on 15/10/2024
+Python program to check  whether the given number is positive or negative
+"""
+
+
+
+
+num=int(input("Enter the number"))
+if num>0:
+     print("The given number :",num,"is positive")
+
+else:
+     print("The given number:",num,"is negative")
